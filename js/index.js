e226ef558ee8ad81a70030f9972ff3fd511404f2
abduvohid-jck <abduvohidmurodov26@gmail.com>
2024-11-26 20:14:40 +0500

    Project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+        width: {
+          DEFAULT: "",
+          desktop: "",
+        },
+        padding: {
+          DEFAULT: "",
+        },
+      },
+      screens: {
+        desktop: "",
+      },
+    },
+  };
+  
