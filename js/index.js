795a3c7b4816a5db13e7320127a323ec6a1824cb
abduvohid-jck <abduvohidmurodov26@gmail.com>
2024-11-26 20:38:29 +0500

    init

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 tailwind.config = {
-    theme: {
-      container: {
-        center: true,
-        width: {
-          DEFAULT: "100%",
-          desktop: "1130px",
-        },
-        padding: {
-          DEFAULT: "23px",
-        },
+  theme: {
+    container: {
+      center: true,
+      width: {
+        DEFAULT: "100%",
+        d: "1200px",
+      },
+      padding: {
+        DEFAULT: "15px",
       },
       screens: {
-        desktop: "1440px",
+        d: "1200px",
       },
     },
-  };
-  
+    screens: {
+      d: "1200px",
+    },
+    extends: {
+      color: {},
+    },
+  },
+};
